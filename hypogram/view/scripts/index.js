11b131f2276620574e9f85fc7a467aab564be764
Adson Filho <117692306+adsonfilho@users.noreply.github.com>
2024-10-09 17:19:36 -0300

    Delete hypogram directory

--- hypogram/view/scripts/index.js
+++
@@ -1,7 +0,0 @@
-import { createHeader } from '../components/header.js';
-import { createFooter } from '../components/footer.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    createHeader();
-    createFooter();
-});
