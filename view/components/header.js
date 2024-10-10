b990119b6522492a95ddcb39b363a95de2ba1f27
Adson Filho <117692306+adsonfilho@users.noreply.github.com>
2024-10-10 19:54:44 -0300

    Add files via upload

--- view/components/header.js
+++ view/components/header.js
@@ -1,18 +1,16 @@
-export function createHeader(){
-
+export function createHeader() {
     const header = document.createElement('header');
-    header.innerHTML =
-    `
+    header.innerHTML = `
     <div class="header-container">
         <img>
         <nav>
             <a href="index.html">Inicio</a>
             <a href="sobre.html">Sobre</a>
             <a href="aprendaScrum.html">Aprenda Scrum</a>
-            <a href="">Questionários</a>
+            <a href="" class="show">Questionários</a>
         </nav>
         <div id="botoes">
-            <button>Certificado</button>
+            <button class="show">Certificado</button>
             <button>Entrar</button>
         </div>
     </div>
@@ -21,6 +19,15 @@ export function createHeader(){
     const img = header.querySelector('img');
     img.src = '../public/imgs/logotipo.svg';
     img.alt = "Logotipo do curso";
+
+    let login = true; 
+    
+    if (!login) {
+        const aparecer = header.querySelectorAll('.show'); 
+        aparecer.forEach(elemento => {
+            elemento.style.display = 'none'; 
+        });
+    }
 
     const main = document.querySelector('main');
     document.body.insertBefore(header, main);
